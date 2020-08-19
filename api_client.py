06c7827dcdbe31b42199b7ffd2eed5054153244e
jbalinski <balinski.jakub@gmail.com>
2020-08-19 23:15:07 +0200

    Code refactoring, small improvements

--- api_client.py
+++ api_client.py
@@ -1,67 +1,87 @@
 import os
 import sys
-
-import requests
 from string import Template
-from multiprocessing import Process
+from time import sleep
+import requests
+from multiprocessing import Pool
 
 from urllib3.exceptions import NewConnectionError
 
-
-def get_size_arg(size: str):
-    if size in ("small", "s"):
-        return "thumbUrl"
-    elif size in ("medium", "m"):
-        return "smallImageUrl"
-    elif size in ("large", "l"):
-        return "imageUrl"
-    else:
-        raise ValueError(f"Argument size has to be one of 'small' ('s'), 'medium' ('m'), 'large' ('l'). Got '{size}'")
+API_ENDPOINT = Template("https://api.thesneakerdatabase.com/v1/sneakers?limit=100&page=$page")
 
 
-def download_images(output_path, size='s', limit=float("inf"), skip_existing=True):
-    size_arg = get_size_arg(size)
+def download_images(output_path: str, img_size="s", limit=float("inf"), starting_page=0):
+    size_arg = get_img_size_arg(img_size)
     os.makedirs(output_path, exist_ok=True)
-    api_endpoint = Template("https://api.thesneakerdatabase.com/v1/sneakers?limit=100&page=$page")
-    page = n_images = 0
+    page = starting_page
+    n_images = 0
+    p = Pool()
 
-    while data := requests.get(api_endpoint.substitute(page=page)).json()["results"]:
-        for entrance in data:
-            if img_url := entrance["media"][size_arg]:
-                p = Process(target=save_image, args=(img_url, output_path, skip_existing))
-                p.start()
-                n_images += 1
-                if n_images == limit:
-                    try:
-                        p.join()
-                    except AssertionError:
-                        pass
-                    return
+    while n_images < limit and (urls := get_image_urls(page, size_arg)):
+        if n_images + len(urls) >= limit:
+            urls = urls[:limit - n_images]
+
+        p.starmap_async(save_image, [(url, output_path) for url in urls])
+        n_images += len(urls)
         page += 1
 
+    p.close()
+    p.join()
 
-def save_image(img_url, output_path, skip_existing=True):
-    filename = img_url.split("/")[-1]
-    if "?" in filename:
-        filename = filename.split("?")[0]
 
+def get_image_urls(page: int, img_size: str) -> list:
+    sneakers = requests.get(API_ENDPOINT.substitute(page=page)).json()["results"]
+    return [img_url for sneaker in sneakers if (img_url := sneaker["media"][img_size])]
+
+
+def save_image(img_url: str, output_path: str):
+    def hold_on(time_in_s):
+        print(f"Next attempt in {time_in_s}s...")
+        sleep(time_in_s)
+
+    for attempts in (1, 2, 3):
+        try:
+            image = requests.get(img_url)
+            break
+        except NewConnectionError:
+            print(f"[{attempts}/3] Failed to fetch image from {img_url} due to connection error.", file=sys.stderr)
+            if attempts < 3:
+                hold_on(5*attempts)
+    else:
+        print(f"All trials failed. Returning...")
+        return
+
+    filename = filename_from_url(img_url)
     filepath = os.path.join(output_path, filename)
-    if skip_existing and os.path.exists(filepath):
-        print(f"File {filename} already exists. Skipping...")
-        return
-
-    try:
-        image = requests.get(img_url)
-    except NewConnectionError:
-        print(f"Failed to fetch image from {img_url} due to connection error.", file=sys.stderr)
-        return
 
     with open(filepath, 'wb') as f:
         print(f"Saving {filename}...")
         f.write(image.content)
 
 
+def get_img_size_arg(img_size: str) -> str:
+    valid_sizes = {
+        ("small", "s"): "thumbUrl",
+        ("medium", "m"): "imageUrl",
+        ("large", "l"): "smallImageUrl"
+    }
+
+    for size_key, size_arg in valid_sizes.items():
+        if img_size in size_key:
+            return size_arg
+    else:
+        raise ValueError(f"Argument size has to be in {list(valid_sizes.keys())}. Got '{img_size}' instead.")
+
+
+def filename_from_url(img_url: str) -> str:
+    filename = img_url.split("/")[-1]
+    if "?" in filename:
+        filename = filename.split("?")[0]
+
+    return filename
+
+
 if __name__ == "__main__":
-    # todo thread pool? cmd args?
+    # todo cmd args?
     print("Downloading all available images...")
     download_images("images")
