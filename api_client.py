20b07f04a024543d0afd07543350787e8270c5d7
jbalinski <balinski.jakub@gmail.com>
2020-08-20 23:33:53 +0200

    Improve api_client

--- api_client.py
+++ api_client.py
@@ -1,62 +1,80 @@
 import os
 import sys
+from multiprocessing import Pool
 from string import Template
 from time import sleep
+from urllib.parse import urlparse
 import requests
-from multiprocessing import Pool
-
 from urllib3.exceptions import NewConnectionError
 
 API_ENDPOINT = Template("https://api.thesneakerdatabase.com/v1/sneakers?limit=100&page=$page")
+inf = sys.maxsize
 
 
-def download_images(output_path: str, img_size="s", limit=float("inf"), starting_page=0):
+def download_images(output_dir: str, img_size="s", limit=inf, starting_page=0):
     size_arg = get_img_size_arg(img_size)
-    os.makedirs(output_path, exist_ok=True)
-    page = starting_page
+    if limit < 0 or starting_page < 0:
+        raise ValueError("Starting page number and limit must not be negative!")
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    pool = Pool()
+    image_urls = setup_image_urls_generator(size_arg, starting_page)
     n_images = 0
-    p = Pool()
 
-    while n_images < limit and (urls := get_image_urls(page, size_arg)):
-        if n_images + len(urls) >= limit:
-            urls = urls[:limit - n_images]
+    while n_images < limit:
+        try:
+            urls = next(image_urls)
+        except StopIteration:
+            break
 
-        p.starmap_async(save_image, [(url, output_path) for url in urls])
+        urls = urls[:min(len(urls), limit - n_images)]
+        pool.starmap_async(save_image, [(url, output_dir) for url in urls])
         n_images += len(urls)
-        page += 1
 
-    p.close()
-    p.join()
+    pool.close()
+    pool.join()
 
-
-def get_image_urls(page: int, img_size: str) -> list:
-    sneakers = requests.get(API_ENDPOINT.substitute(page=page)).json()["results"]
-    return [img_url for sneaker in sneakers if (img_url := sneaker["media"][img_size])]
+    print(f"Successfully downloaded {n_images} images.", file=sys.stderr)
 
 
-def save_image(img_url: str, output_path: str):
-    def hold_on(time_in_s):
-        print(f"Next attempt in {time_in_s}s...")
-        sleep(time_in_s)
-
-    for attempts in (1, 2, 3):
+def save_image(img_url: str, output_dir: str):
+    for attempt in (1, 2, 3):
         try:
             image = requests.get(img_url)
             break
         except NewConnectionError:
-            print(f"[{attempts}/3] Failed to fetch image from {img_url} due to connection error.", file=sys.stderr)
-            if attempts < 3:
-                hold_on(5*attempts)
+            print(f"[{attempt}/3] Failed to fetch image from {img_url} due to connection error.", file=sys.stderr)
+            if attempt < 3:
+                print(f"Next attempt in {5 * attempt}s...")
+                sleep(5 * attempt)
     else:
-        print(f"All trials failed. Returning...")
+        print("All trials failed. Returning...")
         return
 
-    filename = filename_from_url(img_url)
-    filepath = os.path.join(output_path, filename)
+    filename = os.path.basename(urlparse(img_url).path)
+    filepath = os.path.join(output_dir, filename)
 
     with open(filepath, 'wb') as f:
         print(f"Saving {filename}...")
         f.write(image.content)
+
+
+def setup_image_urls_generator(size_arg: str, starting_page: int) -> iter:
+    def image_urls_generator() -> list:
+        page = starting_page
+        while True:
+            sneakers = requests.get(API_ENDPOINT.substitute(page=page)).json()["results"]
+            if not sneakers:
+                print(f"API ran out of sneakers on page {page}.", file=sys.stderr)
+                break
+
+            image_urls = [img_url for sneaker in sneakers if (img_url := sneaker["media"][size_arg])]
+            page += 1
+
+            yield image_urls
+
+    return image_urls_generator()
 
 
 def get_img_size_arg(img_size: str) -> str:
@@ -73,15 +91,7 @@ def get_img_size_arg(img_size: str) -> str:
         raise ValueError(f"Argument size has to be in {list(valid_sizes.keys())}. Got '{img_size}' instead.")
 
 
-def filename_from_url(img_url: str) -> str:
-    filename = img_url.split("/")[-1]
-    if "?" in filename:
-        filename = filename.split("?")[0]
-
-    return filename
-
-
 if __name__ == "__main__":
     # todo cmd args?
     print("Downloading all available images...")
-    download_images("images")
+    download_images("images", starting_page=297)
